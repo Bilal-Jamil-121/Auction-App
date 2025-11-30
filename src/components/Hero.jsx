a60cfc3433303733e2e7ee66c08ae7243ce8b787
Bilal <aliahmadas3366@gmail.com>
2025-11-30 15:42:52 +0500

    improve styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div className='md:flex pt-2 md:px-17 gap-4'>
-      <div className='md:w-1/2 rounded-2xl shadow-xl bg-blue-500 text-white flex flex-col justify-center items-center p-10'>
+    <div className='md:flex pt-2 md:px-17 md:mx-0 mx-3 gap-4'>
+      <div className='md:w-1/2 md:rounded-2xl shadow-xl bg-blue-500 text-white flex flex-col justify-center items-center p-10'>
         <h1 className='text-5xl font-bold mb-6'>Welcome to BidMaster</h1>
             <p className='text-lg mb-8'>Your ultimate platform for online auctions and bidding.</p>
             <button className='bg-white text-blue-500 font-semibold px-6 py-3 rounded hover:bg-gray-200 transition duration-300'> <Link to="/Login">Get Started</Link></button>
       </div>
-      <div className='md:w-1/2 relative overflow-hidden rounded-2xl shadow-xl'>
+      <div className='md:w-1/2 flex items-center justify-center relative md:my-0 h-80 my-2 overflow-hidden md:rounded-2xl shadow-xl'>
   <img
-    className='w-full h-full object-cover transition-transform duration-500 hover:scale-105'
+    className='w-full h-full  object-cover transition-transform duration-500 hover:scale-105'
     src="Bid.jfif"
     alt="Auctions"
   />
